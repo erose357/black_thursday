eb1e6ea8fd271e6448dd53037a5047407abd1bca
erose357 <erose357@gmail.com>
2018-04-21 16:18:29 -0600

    Adds remaining methods for TransactionRepository spec_harness and sanitation passing

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -28,4 +28,16 @@ class TransactionRepository
   def find_by_id(transaction_id)
     @data[transaction_id]
   end
+
+  def find_all_by_invoice_id(invoice_id)
+    @data.values.find_all { |t| t.invoice_id == invoice_id }
+  end
+
+  def find_all_by_credit_card_number(cc_number)
+    @data.values.find_all { |t| t.credit_card_number == cc_number }
+  end
+
+  def find_all_by_result(result)
+    @data.values.find_all { |t| t.result == result }
+  end
 end
--- spec/transaction_repository_spec.rb
+++ spec/transaction_repository_spec.rb
@@ -28,5 +28,65 @@ RSpec.describe TransactionRepository do
         expect(tr.find_by_id(100)).to be_nil
       end
     end
+
+    describe '#find_all_by_invoice_id' do
+      it 'returns one or more matches to invoice_id provided' do
+        transaction = tr.find_all_by_invoice_id(3949)
+
+        expect(transaction.count).to eq(2)
+        expect(transaction).to be_an_instance_of(Array)
+        expect(transaction[0]).to be_an_instance_of(Transaction)
+        expect(transaction.map(&:id)).to eq([2506, 2695])
+      end
+
+      it 'returns [] if no match found' do
+        transaction = tr.find_all_by_invoice_id(100)
+
+        expect(transaction).to eq([])
+      end
+    end
+
+    describe '#find_all_by_credit_card_number' do
+      it 'returns one or more matches to credit card number provided' do
+        transaction = tr.find_all_by_credit_card_number(4230049223634282)
+
+        expect(transaction.count).to eq(1)
+        expect(transaction[0]).to be_an_instance_of(Transaction)
+        expect(transaction).to be_an_instance_of(Array)
+        expect(transaction[0].id).to eq(2287)
+      end
+
+      it 'returns [] if no matches found' do
+        transaction = tr.find_all_by_invoice_id(100)
+
+        expect(transaction).to eq([])
+      end
+    end
+
+    describe '#find_all_by_result' do
+      it "returns one or more matches to 'success' result provided" do
+        transaction = tr.find_all_by_result('success')
+
+        expect(transaction.count).to eq(17)
+        expect(transaction).to be_an_instance_of(Array)
+        expect(transaction[0]).to be_an_instance_of(Transaction)
+        expect(transaction.map(&:id)).to include(2437, 381, 2287)
+      end
+
+      it "returns one or more matches to 'failed' result provided" do
+        transaction = tr.find_all_by_result('failed')
+
+        expect(transaction.count).to eq(6)
+        expect(transaction).to be_an_instance_of(Array)
+        expect(transaction[0]).to be_an_instance_of(Transaction)
+        expect(transaction.map(&:id)).to include(2961, 1709, 608)
+      end
+
+      it 'returns [] if no matches found' do
+        transaction = tr.find_all_by_result(nil)
+
+        expect(transaction).to eq([])
+      end
+    end
   end
 end
